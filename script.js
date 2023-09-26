3ce3f17532871ea3a50656b1f88b348066d0ae92
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2023-09-27 01:04:23 +0200

    lat lng temps reel

--- script.js
+++ script.js
@@ -48,13 +48,31 @@ let startButton = document.getElementById('startButton');
 
 // Tableau des différentes villes
 
-let bordeaux = [44.820, -0.565];
+/* let bordeaux = [44.820, -0.565];
 let lyon = [45.743, 4.842];
 let paris = [48.827, 2.370];
 let marseille = [43.292, 5.369];
 let lille = [50.618, 3.069];
 let strasbourg = [48.577, 7.758];
-let nantes = [47.216, 1.555];
+let nantes = [47.216, 1.555]; */
+
+let villes = [
+    { nom: "Bordeaux", coordonnees: [44.820, -0.565] },
+    { nom: "Lyon", coordonnees: [45.743, 4.842] },
+    { nom: "Paris", coordonnees: [48.827, 2.370] },
+    { nom: "Marseille", coordonnees: [43.292, 5.369] },
+    { nom: "Lille", coordonnees: [50.618, 3.069] },
+    { nom: "Strasbourg", coordonnees: [48.577, 7.758] },
+    { nom: "Nantes", coordonnees: [47.216, 1.555] },
+    { nom: "Toulouse", coordonnees: [43.604, 1.443] },
+    { nom: "Nice", coordonnees: [43.703, 7.266] },
+    { nom: "Rennes", coordonnees: [48.117, -1.677] },
+    { nom: "Montpellier", coordonnees: [43.611, 3.877] },
+    { nom: "Brest", coordonnees: [48.390, -4.486] },
+    { nom: "Grenoble", coordonnees: [45.188, 5.724] },
+    { nom: "Dijon", coordonnees: [47.322, 5.041] },
+    { nom: "Le Havre", coordonnees: [49.493, 0.107] }
+];
 
 
 startButton.onclick= function() {
@@ -62,25 +80,49 @@ startButton.onclick= function() {
 
     switch (start) {
         case 'bordeaux':
-            startCity = bordeaux;
+            startCity = villes[0].coordonnees;
             break;
         case 'lyon':
-            startCity = lyon;
+            startCity = villes[1].coordonnees;
             break;
         case 'paris':
-            startCity = paris;
+            startCity = villes[2].coordonnees;
             break;
         case 'marseille':
-            startCity = marseille;
+            startCity = villes[3].coordonnees;
             break;
         case 'lille':
-            startCity = lille;
+            startCity = villes[4].coordonnees;
             break;
         case 'strasbourg':
-            startCity = strasbourg;
+            startCity = villes[5].coordonnees;
             break;
         case 'nantes':
-            startCity = nantes;
+            startCity = villes[6].coordonnees;
+            break;
+        case 'toulouse':
+            startCity = villes[7].coordonnees;
+            break;
+        case 'nice':
+            startCity = villes[8].coordonnees;
+            break;
+        case 'rennes':
+            startCity = villes[9].coordonnees;
+            break;
+        case 'montpellier':
+            startCity = villes[10].coordonnees;
+            break;
+        case 'brest':
+            startCity = villes[11].coordonnees;
+            break;
+        case 'grenoble':
+            startCity = villes[12].coordonnees;
+            break;
+        case 'dijon':
+            startCity = villes[13].coordonnees;
+            break;
+        case 'lehavre':
+            startCity = villes[14].coordonnees;
             break;
     };
 
@@ -88,25 +130,49 @@ startButton.onclick= function() {
 
     switch (end) {
         case 'bordeaux':
-            endCity = bordeaux;
+            endCity = villes[0].coordonnees;
             break;
         case 'lyon':
-            endCity = lyon;
+            endCity = villes[1].coordonnees;
             break;
         case 'paris':
-            endCity = paris;
+            endCity = villes[2].coordonnees;
             break;
         case 'marseille':
-            endCity = marseille;
+            endCity = villes[3].coordonnees;
             break;
         case 'lille':
-            endCity = lille;
+            endCity = villes[4].coordonnees;
             break;
         case 'strasbourg':
-            endCity = strasbourg;
+            endCity = villes[5].coordonnees;
             break;
         case 'nantes':
-            startCity = nantes;
+            endCity = villes[6].coordonnees;
+            break;
+        case 'toulouse':
+            endCity = villes[7].coordonnees;
+            break;
+        case 'nice':
+            endCity = villes[8].coordonnees;
+            break;
+        case 'rennes':
+            endCity = villes[9].coordonnees;
+            break;
+        case 'montpellier':
+            endCity = villes[10].coordonnees;
+            break;
+        case 'brest':
+            endCity = villes[11].coordonnees;
+            break;
+        case 'grenoble':
+            endCity = villes[12].coordonnees;
+            break;
+        case 'dijon':
+            endCity = villes[13].coordonnees;
+            break;
+        case 'lehavre':
+            endCity = villes[14].coordonnees;
             break;
     };
 
@@ -119,10 +185,30 @@ startDrone = function(startCity, endCity){
 
     console.log(endCity);
 
-    let marker = L.Marker.movingMarker([startCity, endCity], speed).addTo(map);
+    let marker = L.Marker.movingMarker([startCity, endCity, startCity], speed).addTo(map);
+
+    /* document.getElementById('Lat').innerHTML = "";
+    document.getElementById('Lng').innerHTML = ""; */
         
     marker.start();
+
+    function getLatLngEveryTime() {
+
+        document.getElementById('Lat').innerHTML = marker.getLatLng().lat;
+        document.getElementById('Lng').innerHTML = marker.getLatLng().lng;
+        setInterval(function() {
+            window.requestAnimationFrame(getLatLngEveryTime);
+        }, 500);
+    }
+
+    window.requestAnimationFrame(getLatLngEveryTime);
 };
+
+
+
+
+
+
 
 
 
